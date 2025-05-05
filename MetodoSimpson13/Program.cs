12fa92475d32159f24372aa01408a761fbc33e9e
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2025-05-05 14:12:14 -0600

    first commit

---
+++ MetodoSimpson13/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(new string('-', 60));
+Console.WriteLine("Método de Simpson 1/3");
+Console.WriteLine(new string('-', 60));
+
+Console.WriteLine("Seleccione la ecuación a integrar:");
---
+++ MetodoSimpson13/utils/simpson13.cs
@@ -0,0 +1,10 @@
+namespace MetodoSimpson13.utils {
+
+    /// <summary>
+    /// Clase para el método de Simpson 1/3 para aplicar con n puntos
+    /// </summary>
+    /// 
+    class Simpson13 {
+        
+    }
+}
